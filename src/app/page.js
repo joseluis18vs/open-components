bf2b5541d41e628d9dc29f50dd74a930da74ef63
José Luis Vanegas S <joseluis18vs@gmail.com>
2023-05-23 06:45:09 -0600

    Build project

---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+import Carousel from "../components/containers/carousel";
+
+export default function Page()
+{
+    const services = [
+        {
+            service: "",
+            description: "",
+            category: ""
+        }
+    ]
+
+    return(
+        <>
+            <Carousel />
+        </>
+    )
+}
---
+++ src/components/containers/carousel.jsx
@@ -0,0 +1,13 @@
+export default function Carousel()
+{
+    return(
+        <div className="carousel">
+            <div className="card">
+                <div className="card_content">
+                    Servide details
+                </div>
+            </div>
+            <div className="panel">Panel</div>
+        </div>
+    )
+}
